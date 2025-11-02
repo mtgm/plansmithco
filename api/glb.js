1dc4e0db38c3a966250b06b56fafd25ca457ce93
mtgm <serhatyildirim123@gmail.com>
2025-11-02 18:37:44 +0300

    Update glb.js

--- api/glb.js
+++ api/glb.js
@@ -6,7 +6,7 @@ const pump = promisify(pipeline);
 
 const s3 = new S3Client({
   region: "auto",
-  endpoint: `https://${process.env.CF_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: "https://pub-c09e063308e5459cb4c5727415475d43.r2.dev",
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
