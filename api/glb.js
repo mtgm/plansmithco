f299e61bf4c84de13ef98fec51bed1bc9a8bc25e
mtgm <serhatyildirim123@gmail.com>
2025-11-02 18:13:49 +0300

    Update glb.js

--- api/glb.js
+++ api/glb.js
@@ -4,7 +4,7 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const s3 = new S3Client({
   region: "auto",
-  endpoint: `https://${process.env.CF_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: `https://pub-c09e063308e5459cb4c5727415475d43.r2.dev`,
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
