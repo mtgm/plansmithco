885151d87f46187f7c99d216577a102fa2a89b45
mtgm <serhatyildirim123@gmail.com>
2026-02-02 23:11:19 +0300

    -

--- script.js
+++ script.js
@@ -236,7 +236,7 @@ function selectProduct(product) {
             stageWrapper.appendChild(mobileBackBtn);
         }
     }
-    if (mobileBackBtn) mobileBackBtn.style.display = 'flex';
+    // if (mobileBackBtn) mobileBackBtn.style.display = 'flex'; // Removed to allow CSS control
 
     setupVariantTabs(product);
 
